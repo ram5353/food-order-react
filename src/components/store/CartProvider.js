1dd706fee8ea3ef24bce59e47f28aadcbc44f711
ram5353 <ramkiran5353@gmail.com>
2022-06-24 16:32:44 -0400

    added reducer, refs and forward refs

--- src/components/store/CartProvider.js
+++ src/components/store/CartProvider.js
@@ -1,13 +1,39 @@
+import { useReducer } from "react"
 import CartContext from "./cart-contex"
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        const updatedItems = state.items.concat(action.item)
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        console.log(updatedItems)
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+    return defaultCartState
+}
 
 const CartProvider = (props) => {
 
-    const addItemHandler = item => {}
-    const removeItemHandler = id => {}
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+    console.log(cartState);
+
+    const addItemHandler = item => {
+        dispatchCartAction({type: 'ADD', item: item})
+    }
+    const removeItemHandler = id => {
+        dispatchCartAction({type: 'REMOVE', id: id})
+    }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemHandler,
         removeItem: removeItemHandler
     }
